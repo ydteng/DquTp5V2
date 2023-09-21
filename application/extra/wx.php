01d798c29d6eb3ded00e8aea0ebfc680e35c69da
绿树荫下跑猫猫 <35255690+ydteng@users.noreply.github.com>
2023-09-21 13:11:13 +0800

    Update wx.php

--- application/extra/wx.php
+++ application/extra/wx.php
@@ -14,8 +14,8 @@ return [
      * bac033d11bb02ef997f2196bfba226a9
      *
      * */
-    'app_id' => 'wxf6f89449f4c5a634',
-    'app_secret' => '3e722e0b5e89c0e21ddc1efc54844197',
+    'app_id' => 'yours',
+    'app_secret' => 'yours',
     'login_url' => "https://api.weixin.qq.com/sns/jscode2session?".
         "appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
 ];
